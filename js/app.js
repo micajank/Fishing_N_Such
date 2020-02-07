5812dcaf08459ea8326acfc052495468fb6b1311
micajank <micajank@umich.edu>
2020-02-07 00:30:19 -0800

    MVP plus stretch goals. Maybe want to add a better fitting background but too tired to fix it tonight

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ document.querySelector("#restart-button").addEventListener("click", function() {
     window.location.reload();
 })
 
-
 // Make fish spawn at random locations
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -24,31 +23,33 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 };
 
-// Fishing Line
-let fishingLine = null;
-let reelDemFish;
-
-
-
 // Fisherwoman constructor
-function Fisher(x, y, color, width, height) {
+function Fisher(x, y, color, width, height, src) {
     this.x = x;
     this.y = y;
     this.color = color;
     this.width = width;
     this.height = height;
+    this.image = new Image();
+        if (src) {
+            this.image.src = src;
+        }
     this.render = function() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (this.image.src) {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        } else {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 }
 // Create a fish lady
-let fisherwoman = new Fisher(300, 20, "brown", 50, 15);
+let fisherwoman = new Fisher(280, 9, "brown", 80, 50, "img/boat.png");
 
 let direction = -1;
 // Fish constructor
 class Fish {
-    constructor(x, y, color, width, height, points) {
+    constructor(x, y, color, width, height, points, src) {
         this.x = x;
         this.y = y;
         this.color = color;
@@ -57,6 +58,10 @@ class Fish {
         this.caught = false;
         this.points = points;
         this.initDirection = -1;
+        this.image = new Image();
+        if (src) {
+            this.image.src = src;
+        }
         this.findDirection = function() {
             if (Math.random() >= .5) {
                 direction = 1; 
@@ -68,8 +73,13 @@ class Fish {
         };
         this.initDirection = this.findDirection();
         this.render = function() {
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.width, this.height, this.points);
+            if (this.image.src) {
+                ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            }
+            else {
+                ctx.fillStyle = this.color;
+                ctx.fillRect(this.x, this.y, this.width, this.height, this.points);
+            }
         };
         this.velocity = {
             x: Math.random() - 0.5, // Random x value from -0.5 to 0.5
@@ -78,41 +88,86 @@ class Fish {
             // move along x axis
             // check direction
             if (this.initDirection === 0) {
-                if (this.x < 820) {
-                    this.x++;
-                }
-                else {
-                    // check fish
-                    if (this.width === 10) {
-                        // set new starting point and flip direction
-                        this.y = getRandomInt(20, 390);                    
-                        this.initDirection = 1;
-                    }
-                    else if (this.width === 20) {
-                        this.y = getRandomInt(100, 380);                    
-                        this.initDirection = 1;
+                // if not a hazard
+                if (this.width !== 120 && this.width !== 30) {
+                    if (this.x < 820) {
+                        this.x++;
+                        if (this.width === 21) {
+                            this.image.src = "img/babyFish2.png";
+                        }
+                        else if (this.width === 41) {
+                            this.image.src = "img/mediumFish2.png";
+                        }
+                        else if (this.width === 60) {
+                            this.image.src = "img/largeFish2.png";
+                        }
                     }
                     else {
-                        this.y = getRandomInt(200, 360);                    
+                        // check fish
+                        if (this.width === 21) {
+                            // set new starting point and flip direction
+                            this.y = getRandomInt(20, 379);                    
+                            this.initDirection = 1;
+                        }
+                        else if (this.width === 41) {
+                            this.y = getRandomInt(100, 339);                    
+                            this.initDirection = 1;
+                        }
+                        else {
+                            this.y = getRandomInt(200, 340);                    
+                            this.initDirection = 1;
+                        }
+                    }
+                }
+                //if a hazard
+                else {
+                    if (this.x < 1000) {
+                        this.x++;
+                        this.image.src = "img/BigShark.png";
+                    }
+                    else {
+                        this.y = getRandomInt(20, 400);                    
                         this.initDirection = 1;
                     }
                 }
             } 
             else {
-                if (this.x > -50) {
-                    this.x--;
-                }
-                else {
-                    if (this.width === 10) {
-                        this.y = getRandomInt(40, 390);
-                        this.initDirection = 0;
-                    }
-                    else if (this.width === 20) {
-                        this.y = getRandomInt(100, 380);                    
-                        this.initDirection = 0;
+                // if not a hazard
+                if (this.width !== 120 && this.width !== 30) {
+                    if (this.x > -50) {
+                        this.x--;
+                        if (this.width === 21) {
+                            this.image.src = "img/babyFish.png";
+                        }
+                        else if (this.width === 41) {
+                            this.image.src = "img/mediumFish.png";
+                        } 
+                        else if (this.width === 60) {
+                            this.image.src = "img/largeFish.png";
+                        }
                     }
                     else {
-                        this.y = getRandomInt(200, 360);                    
+                        if (this.width === 21) {
+                            this.y = getRandomInt(40, 379);
+                            this.initDirection = 0;
+                        }
+                        else if (this.width === 41) {
+                            this.y = getRandomInt(100, 339);                    
+                            this.initDirection = 0;
+                        }
+                        else {
+                            this.y = getRandomInt(200, 340);                    
+                            this.initDirection = 0;
+                        }
+                    }
+                } 
+                else {
+                    if (this.x > -200) {
+                        this.x--;
+                        this.image.src = "img/BigShark2.png";
+                    }
+                    else {
+                        this.y = getRandomInt(40, 390);
                         this.initDirection = 0;
                     }
                 }
@@ -123,17 +178,20 @@ class Fish {
     }
 }
 
+
 //Need to fill arraw with Fish to keep code DRY
 fishArr = [];
 function createFish() {
+    fishArr.push(new Fish(getRandomInt(-100, 900), getRandomInt(40, 400), "black", 120, 60, 0, "img/BigShark.png"));
+    fishArr.push(new Fish(getRandomInt(-20, 830), getRandomInt(40, 400), "black", 30, 15, -10, "img/BigShark.png"));
     for (let i = 0; i < 15; i++) {
-        fishArr.push(new Fish(getRandomInt(-10, 800), getRandomInt(40, 390), "#FFA500", 10, 10, 1));
+        fishArr.push(new Fish(getRandomInt(-10, 800), getRandomInt(40, 390), "#FFA500", 21, 16, 1, 'img/babyFish.png'));
     }
     for (let i = 0; i < 6; i++) {
-        fishArr.push(new Fish(getRandomInt(-10, 800), getRandomInt(100, 390), "#FF6347", 20, 20, 3));
+        fishArr.push(new Fish(getRandomInt(-10, 800), getRandomInt(100, 390), "#FF6347", 41, 30, 3, "img/mediumFish.png"));
     }
     for (let i = 0; i < 6; i++) {
-        fishArr.push(new Fish(getRandomInt(-10, 800), getRandomInt(200, 390), "#FFD700", 40, 40, 8));
+        fishArr.push(new Fish(getRandomInt(-10, 800), getRandomInt(200, 390), "#FFD700", 60, 50, 8, "img/largeFish.png"));
     }
     
     for (let i = 0; i < fishArr.length; i++) {
@@ -141,8 +199,13 @@ function createFish() {
     }
 }
 
+// Fishing Line
+let fishingLine = null;
+let reelDemFish;
 // Hook
 let hook;
+// Hook status
+let hookIsSet = false;
 
 // create fishing line
 function createFishingLine(e) {
@@ -154,16 +217,17 @@ function createFishingLine(e) {
             end_y: e.offsetY,
             color: "white"
             
+            
         }
     } 
-   hook = new Fisher(fishingLine.end_x, fishingLine.end_y, "black", 5, 5);
+   hook = new Fisher(fishingLine.end_x, fishingLine.end_y, "black", 15, 20, "img/hook.png");
 }
 
 // draw the fishing line
 function drawFishingLine() {
     ctx.beginPath();
     ctx.moveTo(fishingLine.start_x, fishingLine.start_y);
-    ctx.lineTo(fishingLine.end_x, fishingLine.end_y);
+    ctx.lineTo(fishingLine.end_x + 7, fishingLine.end_y + 7);
     ctx.closePath();
     ctx.strokeStyle = fishingLine.color;
     ctx.stroke();
@@ -171,9 +235,11 @@ function drawFishingLine() {
 
     // Adding hook to end of fishing line
     hook.render();
+    hook.image.src = "img/hook.png";
     
 }
 
+let sharkCaught = false;
 var totalPoints = 0;
 // Function that pulls fishin line back in
 function decreaseLineLength(currentX, currentY, endX, endY) {
@@ -181,47 +247,53 @@ function decreaseLineLength(currentX, currentY, endX, endY) {
     let yDecrementVal = 7;
     if (currentY < 20) {
         hookIsSet = false;
-        // MAYBE CALL FUNCTION THAT DOES ALL THIS
-
-        // remove caught fish from fishing array
         if (caughtFishIndex > -1) {
             // update users points
             totalPoints += fishArr[caughtFishIndex].points;
-            document.querySelector("#bottom-right h3").textContent = `Points: ${totalPoints}`;
-            fishArr.splice(caughtFishIndex, 1);
-            caughtFishIndex = -1;
+            // BigShark is caught
+            if (caughtFishIndex === 0) {
+                totalPoints = -1000000;
+                document.querySelector("#bottom-right h3").textContent = `Points: ${totalPoints}`;
+                sharkCaught = true;
+            }
+            // LilShark is caught
+            else if (caughtFishIndex === 1) {
+                document.querySelector("#bottom-right h3").textContent = `Points: ${totalPoints}`;
+                caughtFishIndex = -1;
+            }
+            // Fish is caught
+            else {
+                document.querySelector("#bottom-right h3").textContent = `Points: ${totalPoints}`;
+                // remove caught fish from fishing array
+                fishArr.splice(caughtFishIndex, 1);
+                caughtFishIndex = -1;
+            }
         }
-        // make fish stop following fishing line
-            // reset caughtFish
-            caughtFish = null;
-        // checkwin -  maybe should do this in my gameLoop??  
-        
-        
+        // make fish stop following fishing line / reset caughtFish
+        caughtFish = null;   
     } 
     else {
         if (currentX < endX) {
             fishingLine.end_x += xDecrementVal;
             fishingLine.end_y -= yDecrementVal;
-            hook = new Fisher(fishingLine.end_x, fishingLine.end_y, "black", 5, 5);
+            hook = new Fisher(fishingLine.end_x, fishingLine.end_y, "black", 15, 20, "img/hook.png");
             // Render hook to follow line
             hook.render();
             // Caught fish set to same coords as hook and line end, then rendered to follow
-            caughtFish.y = fishingLine.end_y;
-            caughtFish.x = fishingLine.end_x;
+            caughtFish.y = fishingLine.end_y - 7;
+            caughtFish.x = fishingLine.end_x - 7;
             caughtFish.render();
         } else {
             fishingLine.end_x -= xDecrementVal;
             fishingLine.end_y -= yDecrementVal;
-            hook = new Fisher(fishingLine.end_x, fishingLine.end_y, "black", 5, 5);
+            hook = new Fisher(fishingLine.end_x, fishingLine.end_y, "black", 15, 20, "img/hook.png");
             hook.render();
-            caughtFish.y = fishingLine.end_y;
-            caughtFish.x = fishingLine.end_x;
+            caughtFish.y = fishingLine.end_y - 7;
+            caughtFish.x = fishingLine.end_x - 7;
             caughtFish.render();
         }
     }
 }
-
-let hookIsSet = false;
 
 // moved parameter e into the event listener in an anonymous functiona and then call this function 
 function setHookStartPosition(e) {
@@ -240,20 +312,19 @@ var caughtFish;
 var caughtFishIndex = -1;
 //let caughtFish = [];
 function detectHit() {
+
     for (let i = 0; i < fishArr.length; i++) {
-        if (fishArr[i].x <= hook.x + hook.width &&
-            fishArr[i].x + fishArr[i].width >= hook.x &&
-            fishArr[i].y <= hook.y + hook.height &&
-            fishArr[i].y + fishArr[i].height >= hook.y) {
+        if (fishArr[i].x < hook.x + hook.width &&
+            fishArr[i].x + fishArr[i].width > hook.x &&
+            fishArr[i].y < hook.y + hook.height &&
+            fishArr[i].y + fishArr[i].height > hook.y) {
+            fishArr[i].caught = true;
                 
-                fishArr[i].caught = true;
-                if (fishArr[i].caught === true) {
-                    caughtFish = fishArr[i];
-                    caughtFishIndex = i;
-                    console.log(caughtFish);
-                }
-                console.log("caught");
-                console.log(i);
+            if (fishArr[i].caught === true) {
+                caughtFish = fishArr[i];
+                caughtFishIndex = i;
+                console.log(caughtFish);
+            }
         }
     }
 }
@@ -268,15 +339,13 @@ const STARTING_TIME = 30;
 let remainingTime = STARTING_TIME;
 let countdown = null;
 
-
 let updateClock = function() {
-    //TODO: count down in miliseconds
+    // count down in miliseconds
     if (remainingTime > 0) {
         remainingTime--;
     } 
     document.querySelector("#bottom-left h3").textContent = `00:00:${remainingTime < 10 ? "0" + remainingTime : remainingTime}`;
 };
-
 
 function winGame() {
     clearInterval(reelDemFish);
@@ -284,7 +353,7 @@ function winGame() {
     document.querySelector("#bottom-left h3").textContent = 'WINNER WINNER SUSHI DINNER';
 }
 
-let gameOverFish = new Fish(800, 175, "white", 70, 70, 0);
+let gameOverFish = new Fish(800, 125, "white", 150, 150, 0, 'img/game-over-fish.png');
 let lose = false;
 function gameOver() {
     
@@ -300,9 +369,7 @@ function gameOver() {
     else {
         gameOverFish.x = 800;
     }
-    // tell user to click start to play again
 }
-
 
 // Initialize game
 function initGame() {
@@ -311,12 +378,13 @@ function initGame() {
     countdown = setInterval(updateClock, 1000);
     //  create fish and board
     createFish();
-
 }
 
 let animationRequest = null;
 
 initGame();
+
+
 
 // Makes game animate via looping via requestAnimationFrame() function
 function gameLoop() {
@@ -325,9 +393,11 @@ function gameLoop() {
     ctx.clearRect(0, 0, game.width, game.height);
     
     fishArr.forEach((fish, i) => {
+
         fishArr[i].update();
         fish.render();
     });
+    
 
     fisherwoman.render();
     
@@ -341,6 +411,10 @@ function gameLoop() {
         game.removeEventListener('click', setHookStartPosition);
     }
 
+    if (sharkCaught) {
+        gameOver();
+    }
+
     if (remainingTime === 0 && totalPoints < 30) {
         // End game as loser
             gameOver();
@@ -351,18 +425,11 @@ function gameLoop() {
 
     // check if line has been reeled back in
     if (!hookIsSet && !lose) {
-        console.log("Helllllllo");
         clearInterval(reelDemFish);
         game.addEventListener('mousemove', createFishingLine);
         game.addEventListener('click', setHookStartPosition);
     }
-
     animationRequest = requestAnimationFrame(gameLoop);
-    
 }
 
 gameLoop();
-
-// reset game
-    // reset points
-    // reset fish array
